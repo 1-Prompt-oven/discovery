6a342f7c37f9cda0067e3c41310a13ee25f76a1f
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-10-24 17:32:43 +0900

    :wrench: Config Server 사용할 수 있도록 업데이트 완료

--- src/test/java/com/promptoven/discovery/DiscoveryApplicationTests.java
+++ src/test/java/com/promptoven/discovery/DiscoveryApplicationTests.java
@@ -1,13 +1,23 @@
 package com.promptoven.discovery;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DiscoveryApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private Environment env;
 
+    // @Test
+    // void contextLoads() {
+    // }
+
+    @Test
+    void printTestValue() {
+        String testValue = env.getProperty("test.value");
+        System.out.println(testValue);
+    }
 }
